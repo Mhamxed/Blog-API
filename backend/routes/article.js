544741c0c95cb8b10786b7b792f19c69a54d87a4
MHamed <aitsidiouali@gmail.com>
2025-03-05 14:55:57 +0000

    create and implemented all the routes for the comment and the article route

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const articleController = require('../controllers/article.js')
+const verifyJWT = require('../middleware/auth.js')
 const article = Router()
 
 article.get('/api/posts', articleController.posts)
-article.get('/api/articles/:id/comments', articleController.commentsByArticleId)
-article.get('/api/articles/:id', articleController.postById)
-article.post('/api/articles/create', articleController.createArticle)
-article.put('/api/articles/:id/edit', articleController.editArticle)
-article.delete('/api/articles/:id/delete', articleController.deleteArticle)
+article.get('/api/articles/:id/comments', verifyJWT, articleController.commentsByArticleId)
+article.get('/api/articles/:id', verifyJWT, articleController.postById)
+article.post('/api/articles/create', verifyJWT, articleController.createArticle)
+article.put('/api/articles/:id/edit', verifyJWT, articleController.editArticle)
+article.delete('/api/articles/:id/delete', verifyJWT, articleController.deleteArticle)
 
 module.exports = article
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const commentController =  require('../controllers/comment.js')
+const verifyJWT = require('../middleware/auth.js')
 
 const comment = Router()
-comment.get('/api/artciles/:articleid/comments/create', commentController.createComment)
-comment.put('/api/comments/:id/edit', commentController.editComment)
-comment.get('/api/comments/:id/delete', commentController.createComment)
+comment.get('/api/artciles/:articleid/comments/', verifyJWT, commentController.getArticleCommentsById)
+comment.get('/api/comments/:id', verifyJWT, commentController.getCommentById)
+comment.post('/api/artciles/:articleid/comments/create', verifyJWT, commentController.createComment)
+comment.put('/api/comments/:id/edit', verifyJWT, commentController.editComment)
+comment.delete('/api/comments/:id/delete', verifyJWT, commentController.deleteComment)
 
 module.exports = comment
